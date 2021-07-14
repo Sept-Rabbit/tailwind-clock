3b999b58811ccd76f7d50c199b393912cd10d92e
yc999333 <yc99993333@gmail.com>
2021-07-14 14:52:43 +0800

    fix lap id

--- src/components/AddStopWatch.jsx
+++ src/components/AddStopWatch.jsx
@@ -5,7 +5,7 @@ function AddStopWatch(props) {
 
   return (
     <div className="flex justify-between h-7 w-64 my-3 p-2 border-b border-gray-500">
-      <p className="text-sm mx-2">Lap : {id}</p>
+      <p className="text-sm mx-2">Lap : {id + 1}</p>
       <p className="text-sm mx-2">
         {m} : {s} : {ms}
       </p>
