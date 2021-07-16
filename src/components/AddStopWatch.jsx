a2c09a91b0c6f90c230d4c621486b6366c230018
yc999333 <yc99993333@gmail.com>
2021-07-16 23:15:03 +0800

    fix stopwatch lap tilt

--- src/components/AddStopWatch.jsx
+++ src/components/AddStopWatch.jsx
@@ -7,8 +7,9 @@ function AddStopWatch(props) {
     <div className="flex justify-between h-7 w-64 my-3 p-2 border-b border-gray-500">
       <div className="grid grid-cols-2">
         <div className="text-sm mx-2">Lap : {id + 1}</div>
-        <div className="flex flex-row w-20 text-sm mx-7">
-          {m} : {s} : {ms}
+        <div className="flex flex-row w-24 text-sm mx-7">
+          <div className="mx-2">{m}</div> : <div className="mx-2">{s}</div> :{" "}
+          <div className="mx-2">{ms}</div>
         </div>
       </div>
     </div>
