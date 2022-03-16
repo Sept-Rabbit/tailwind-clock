da83563522a794e9956db174797ebf469fd6f0fe
搋兔 <naruto6404@hotmail.com>
2022-03-16 09:41:45 +0800

    update layout flex

--- src/Layout/Layout.jsx
+++ src/Layout/Layout.jsx
@@ -4,12 +4,14 @@ import Header from "./Header";
 
 function Layout(props) {
   return (
-    <div className="h-screen mx-auto font-bold text-white bg-gray-900 rounded-lg sm:w-full lg:w-96">
-      <header>
+    <div className="h-screen mx-auto font-bold text-white bg-gray-900 rounded-lg sm:w-full lg:w-96 flex flex-col">
+      <header className="flex-none">
         <Header />
       </header>
-      <main>{props.children}</main>
-      <footer>
+      <main className="flex-1">
+        {props.children}
+      </main>
+      <footer className="flex-none">
         <Footer />
       </footer>
     </div>
